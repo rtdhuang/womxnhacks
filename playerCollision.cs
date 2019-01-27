cb32c36d658a199794e46bdd91c38ae4f6962357
rtdhuang <47085522+rtdhuang@users.noreply.github.com>
2019-01-27 09:02:03 -0800

    Add files via upload

---
+++ playerCollision.cs
@@ -0,0 +1,20 @@
+﻿
+using UnityEngine;
+
+public class playerCollision : MonoBehaviour {
+
+    public playerMovement movement;
+
+
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        
+        if (collisionInfo.collider.tag == "Obstacle")
+        {
+            movement.enabled = false;
+        }
+
+    }
+
+}
+
---
+++ playerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    //this is for to call it rigidBody
+
+    public float forwardForce = 200f;
+    public float sidewaysForce = 500f;
+
+    // FixedUpdate is to mess with Unity Physics 
+    void FixedUpdate() //people just like it 
+    {
+        //Add a forwardForce
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+        }
+        if(Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+        }
+    }
+}
